fa16c8dc9f0c8f5018ccf0a98799fd039d8a8b5c
San tol <tolsan42@gmail.com>
2026-01-14 00:28:54 +0700

    Create active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,41 @@
-import Link from 'next/link'
-import { AiFillBug } from 'react-icons/ai'
+'use client'
+
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classname from "classname";
 
 const NavBar = () => {
-
+    const currentPath = usePathname();
     const links = [
-        { label: "Dashboard", href: '/' },
-        { label: "Issue", href: '/issue' }
-    ]
+        { label: "Dashboard", href: "/" },
+        { label: "Issue", href: "/issue" },
+    ];
+
+
     return (
-        <nav className='flex space-x-6 px-5 h-14 mb-5 border-b border-black/10 items-center'>
-            <Link href='/'><AiFillBug/></Link>
-            <ul className='flex space-x-6 '>
-                {links.map((link) => (<Link  key={link.href} href={link.href} className=' text-zinc-500 hover:text-zinc-800 transition-colors' >{link.label}</Link>))}
+        <nav className="flex space-x-6 px-5 h-14 mb-5 border-b border-black/10 items-center">
+            <Link href="/">
+                <AiFillBug />
+            </Link>
+            <ul className="flex space-x-6 ">
+                {links.map((link) => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+
+                        className={classname({
+                            'text-zinc-900': currentPath === link.href,
+                            'text-zinc-500': currentPath !== link.href,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
+                    >
+                        {link.label}
+                    </Link>
+                ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
---
+++ classname.d.ts
@@ -0,0 +1,4 @@
+declare module 'classname' {
+    function classname(value: Record<string, boolean | undefined>): string;
+    export = classname;
+}
