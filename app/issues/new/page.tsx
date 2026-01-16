160743ae88a96691fc3e5d3ed741cce1a748bbf3
San tol <tolsan42@gmail.com>
2026-01-16 13:38:04 +0700

    Add react-simplemde-editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,19 @@
-import { TextArea, TextField, Button } from '@radix-ui/themes'
-import React from 'react'
+
+'use client';
+import { TextField, Button } from '@radix-ui/themes'
+import dynamic from 'next/dynamic';
+import "easymde/dist/easymde.min.css";
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+    ssr: false,
+});
+
 
 const NewIssuePage = () => {
     return (
         <div className='max-w-xl space-y-3'>
-            <TextField.Root placeholder='Title'/>
-            <TextArea placeholder='Description'/>
+            <TextField.Root placeholder='Title' />
+            <SimpleMDE placeholder='Description' />
             <Button>Submit New Issue</Button>
         </div>
 
