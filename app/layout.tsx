c475b0798739a2c52fadc1015fc077f92e07d410
San tol <tolsan42@gmail.com>
2026-01-16 13:10:31 +0700

    Config Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+
+const inter = Inter({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,8 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Theme>
+        className={`${inter.variable} antialiased`}>
+        <Theme accentColor="jade">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
